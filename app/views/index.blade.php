7d2a0933e6194000590e735cfe4652c740399ab2
Bogdan Kootz <bodyakootz@gmail.com>
2015-05-18 10:33:26 +0300

    comfy and allo with variables

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -5,7 +5,7 @@
 @extends('partials/footer')
 @extends('partials/left_sidebar')
 {{-- @extends('partials/right_sidebar') --}}
-@extends('partials/catalog_blocks_allo')
+@extends('partials/catalog_blocks_comfy')
 
 
 
